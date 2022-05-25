4abc5dc579f0693332637b6cd6bdc4cf989b4aa9
Tenteur592 <vianneyleman592@gmail.com>
2022-05-25 19:07:59 +0200

    Optimisation

--- Reward/Bing/VerifyIntegrity.py
+++ Reward/Bing/VerifyIntegrity.py
@@ -1,30 +1,69 @@
 import os
 
+#get if pyautogui is installed
+try:
+    import pyautogui
+except:
+    print("pyautogui isn't installed yet, downloading it")
+    os.system("pip install pyautogui")
+    print("pyautogui is now installed")
+
+import pyautogui
 Useless = 0
 
-with open('preferences', 'r') as file:
-    preferences = file.read().splitlines()
-    lang = preferences[0].split(':')[1]
-    browser = preferences[1].split(':')[1]
-    webBrowserIconColomn = preferences[2].split(':')[1]
-    
-    if lang == 'en' or lang == 'fr':
-        Useless = 0
-    else:
-        with open('preferences', 'w') as file:
-            file.write('lang:en\nbrowser:'+browser+'\nwebBrowserIconColomn:'+webBrowserIconColomn)
-    if browser == 'chrome' or browser == 'firefox' or browser == 'edge' or browser == 'opera':
-        Useless = 0
-    else:
-        with open('preferences', 'w') as file:
-            file.write('lang:'+lang+'\nbrowser:firefox\nwebBrowserIconColomn:'+webBrowserIconColomn)
-    if webBrowserIconColomn.isdigit():
-        webBrowserIconColomn = int(webBrowserIconColomn)
-        if webBrowserIconColomn >= 0 and webBrowserIconColomn <= 10:
+#verify if 'preferences' file exists and if not create it with default values (lang:en, browser:firefox, webBrowserIconColomn:0)
+if os.path.exists('preferences'):
+    with open('preferences', 'r') as file:
+        preferences = file.read().splitlines()
+        lang = preferences[0].split(':')[1]
+        browser = preferences[1].split(':')[1]
+        webBrowserIconColomn = preferences[2].split(':')[1]
+        isPath = preferences[3].split(':')[1]
+        browserPath = preferences[4].split(':')[1]
+
+        if lang == 'en' or lang == 'fr':
             Useless = 0
         else:
             with open('preferences', 'w') as file:
-                file.write('lang:'+lang+'\nbrowser:' + browser+'\nwebBrowserIconColomn:0')
-    else:
-        with open('preferences', 'w') as file:
-            file.write('lang:'+lang+'\nbrowser:'+browser+'\nwebBrowserIconColomn:0')
+                file.write('lang:en\nbrowser:'+browser +
+                        '\nwebBrowserIconColomn:'+webBrowserIconColomn+'\nisPath:'+isPath+'\nbrowserPath:'+browserPath)
+        if browser == 'chrome' or browser == 'firefox' or browser == 'edge' or browser == 'opera':
+            Useless = 0
+        else:
+            with open('preferences', 'w') as file:
+                file.write(
+                    'lang:'+lang+'\nbrowser:firefox\nwebBrowserIconColomn:'+webBrowserIconColomn+'\nisPath:'+isPath+'\nbrowserPath:'+browserPath)
+        if webBrowserIconColomn.isdigit():
+            webBrowserIconColomn = int(webBrowserIconColomn)
+            if webBrowserIconColomn >= 0 and webBrowserIconColomn <= 10:
+                Useless = 0
+            else:
+                with open('preferences', 'w') as file:
+                    file.write('lang:'+lang+'\nbrowser:' +
+                            browser+'\nwebBrowserIconColomn:0'+'\nisPath:'+isPath+'\nbrowserPath:'+browserPath)
+        else:
+            with open('preferences', 'w') as file:
+                file.write('lang:'+lang+'\nbrowser:' +
+                        browser+'\nwebBrowserIconColomn:0'+'\nisPath:'+isPath+'\nbrowserPath:'+browserPath)
+        if isPath == 'True' or isPath == 'False':
+            Useless = 0
+        else :
+            with open('preferences', 'w') as file:
+                file.write('lang:'+lang+'\nbrowser:' +
+                        browser+'\nwebBrowserIconColomn:'+webBrowserIconColomn+'\nisPath:False'+'\nbrowserPath:'+browserPath)
+        if browserPath.endswith('firefox.exe') or browserPath.endswith('chrome.exe') or browserPath.endswith('opera.exe') or browserPath.endswith('edge.exe'):
+            Useless = 0
+        else:
+            with open('preferences', 'w') as file:
+                file.write('lang:'+lang+'\nbrowser:' +
+                        browser+'\nwebBrowserIconColomn:'+webBrowserIconColomn+'\nisPath:'+isPath+'\nbrowserPath:C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+else:
+    with open('preferences', 'w') as file:
+        file.write("lang:en\n")
+        file.write("browser:firefox\n")
+        file.write("webBrowserIconColomn:0\n")
+        file.write("isPath:False\n")
+        file.write("browserPath:C:\\Program Files\\Mozilla Firefox\\firefox.exe\n")
+        file.close()
+
+
--- Reward/Bing/recherches bing.py
+++ Reward/Bing/recherches bing.py
@@ -4,8 +4,23 @@ import random
 import time
 from multiprocessing.connection import wait
 
+i = int(0)
+while i < 3:
+    os.system("VerifyIntegrity.py")
+    i += 1
+
+#ask the user what number of searches he wants to do nad id not an integer re ask
+while True:
+    try:
+        numberOfSearches = int(input("How many searches do you want to do? "))
+        break
+    except ValueError:
+        print("Please enter a number")
+print("everything is ok, starting the program")
+
 import pyautogui
 
+pyautogui.FAILSAFE = False
 
 def FIsMatching():
     matches = False
@@ -17,13 +32,19 @@ def FIsMatching():
             matches = False
 
 def GIsMatching():
+    pxielx = 29
+    pxiely = 18
     matches = False
     while matches == False:
-        if pyautogui.pixelMatchesColor(30, 15, (16, 110, 191, 255)):
+        if pyautogui.pixelMatchesColor(pxielx, pxiely, (17, 123, 214)):
             pyautogui.moveTo(450, 250)
             matches = True
         else:
             matches = False
+            #print the current color of the pixel
+            print(pyautogui.pixel(pxielx, pxiely))
+            #do a point where the pixel is
+            pyautogui.moveTo(pxielx, pxiely)
 
 def EIsMatching():
     matches = False
@@ -34,54 +55,65 @@ def EIsMatching():
         else:
             matches = False
 
-
-i = int(0)
-while i < 3:
-    os.system("VerifyIntegrity.py")
-    i += 1
-
-print("everything is ok, starting the program")
-
 with open('preferences', 'r') as file:
         preferences = file.read().splitlines()
         lang = preferences[0].split(':')[1]
         browser = preferences[1].split(':')[1]
         webBrowserIconColomn = int(preferences[2].split(':')[1])
+        isPath = preferences[3].split(':')[1]
+        browserPath = preferences[4].split(':')[1]
 
 PixelResult = 120 + (webBrowserIconColomn * 50)
 
-
 pyautogui.click(PixelResult, 745)
 
-#wait 1.5s
 time.sleep(1.5)
+
+def RandomWord():
+    random_word = ''.join(random.choice(
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))
+    pyautogui.typewrite(random_word, interval=0.05)
+    pyautogui.press("enter")
+
+def EnterBing():
+    pyautogui.typewrite("bing.com")
+    pyautogui.press("enter")
+
+def TypeEnterSpam():
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('backspace')
+    pyautogui.keyUp('ctrl')
+    random_word = ''.join(random.choice(
+            'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))
+    pyautogui.typewrite(random_word, interval=0.01)
+    pyautogui.press("enter")
+
 
 if browser == 'firefox':
     #click on the search bar
     pyautogui.click(300, 65)
-
     #enter the word "bing" and press enter
-    pyautogui.typewrite("bing")
-    pyautogui.press("enter")
-
-    #get the 20, 20 pixel color and if its rgba(16,110,191,255) then set the mouse to the pixel: 450, 250 and if not print the color with rgba and ait 0.5s and repeat
+    EnterBing()
     FIsMatching()
-
-    #enter random word with a-z A-Z 0-9 and with 10 characters
-    random_word = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))
-    pyautogui.typewrite(random_word, interval=0.05)
-    pyautogui.press("enter")
-
+    RandomWord()
     #do a loop that repeat 30 times so the cursor move first to the pixel 400, 150 then press backspace 10 times then redo the same thing of the random word but wait 1s after each times
-
-    for i in range(5):
+    for i in range(numberOfSearches):
         pyautogui.click(400, 150)
-        pyautogui.keyDown('ctrl')
-        pyautogui.press('backspace')
-        pyautogui.keyUp('ctrl')
-        random_word = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))
-        pyautogui.typewrite(random_word, interval=0.01)
-        pyautogui.press("enter")
-
+        TypeEnterSpam()
         FIsMatching()
 
+elif browser == 'chrome':
+    #click on the search bar
+    pyautogui.click(330, 50)
+    #enter the word "bing" and press enter
+    EnterBing()
+    #get the 20, 20 pixel color and if its rgba(16,110,191,255) then set the mouse to the pixel: 450, 250 and if not print the color with rgba and ait 0.5s and repeat
+    GIsMatching()
+    #enter random word with a-z A-Z 0-9 and with 10 characters
+    RandomWord()
+    #do a loop that repeat 30 times so the cursor move first to the pixel 400, 150 then press backspace 10 times then redo the same thing of the random word but wait 1s after each times
+    for i in range(numberOfSearches):
+        pyautogui.click(350, 110)
+        TypeEnterSpam()
+
+        GIsMatching()
